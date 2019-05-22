3d9092f444f9ac4e0523de9208695de615becad0
codernr <zoltan.csore@codernr.com>
2019-05-22 15:31:02 +0200

    Add tsconfig

--- index.ts
+++ index.ts
@@ -6,14 +6,14 @@ export function MockComponent(obj: Component, interfaceImplementations?: object[
 
     if (obj.outputs) {
         obj.outputs.forEach(item => {
-            NgxMockComponent.prototype[item] = new EventEmitter<any>();
+            (NgxMockComponent as any).prototype[item] = new EventEmitter<any>();
         });
     }
 
     if (interfaceImplementations) {
-        interfaceImplementations.forEach(i => {
-            Object.keys(i).forEach(k => {
-                NgxMockComponent.prototype[k] = i[k];
+        interfaceImplementations.forEach((i: object) => {
+            Object.keys(i).forEach((k: any) => {
+                (NgxMockComponent as any).prototype[k] = (i as any)[k];
             });
         });
     }
